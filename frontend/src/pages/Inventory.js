21bf6d054f5793c382bd34fd6b81c8efcd3956c2
I Putu Bayu Aditya Putra <49913455+Bayuaditya24@users.noreply.github.com>
2025-06-21 12:39:43 +0800

    Update Inventory.js

--- frontend/src/pages/Inventory.js
+++ frontend/src/pages/Inventory.js
@@ -1,26 +1,14 @@
-import Navigation from "../components/Navigation";
-import { Link } from "react-router-dom";
-import { IoHome } from "react-icons/io5";
-import { Col } from "react-bootstrap";
-// Assuming this is the correct path for your ListPembelian component
 import Gudang from "../components/inventory/Gudang";
+import MainLayout from "../components/MainLayout";
+import BreadcrumbHeader from "../components/BreadcrumbHeader";
 
 const Inventory = () => {
   return (
     <>
-      <Navigation />
-      <Col
-        className="mb-3 p-2 card d-block"
-        style={{ background: "white", color: "grey" }}
-      >
-        <small className="m-1">
-          <Link style={{ textDecoration: "none", color: "grey" }} to={"/"}>
-            <IoHome className="mb-1" /> Home /
-          </Link>
-        </small>
-        <small className="m-1">Inventory</small>
-      </Col>
-      <Gudang />
+      <MainLayout>
+        <BreadcrumbHeader title="Inventory" />
+        <Gudang />
+      </MainLayout>
     </>
   );
 };
