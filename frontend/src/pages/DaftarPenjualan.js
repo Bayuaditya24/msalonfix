66bed83d27d07b9cb69bd3c86bbb3ded6a23f8bc
I Putu Bayu Aditya Putra <49913455+Bayuaditya24@users.noreply.github.com>
2025-06-21 12:39:03 +0800

    Update DaftarPenjualan.js

--- frontend/src/pages/DaftarPenjualan.js
+++ frontend/src/pages/DaftarPenjualan.js
@@ -1,25 +1,14 @@
-import Navigation from "../components/Navigation";
 import ListPenjualan from "../components/kasir/ListPenjualan";
-import { Link } from "react-router-dom";
-import { IoHome } from "react-icons/io5";
-import { Col } from "react-bootstrap";
+import MainLayout from "../components/MainLayout";
+import BreadcrumbHeader from "../components/BreadcrumbHeader";
 
 const DaftarPenjualan = () => {
   return (
     <>
-      <Navigation />
-      <Col
-        className="mb-3 p-2 card d-block"
-        style={{ background: "white", color: "grey" }}
-      >
-        <small className="m-1">
-          <Link style={{ textDecoration: "none", color: "grey" }} to={"/"}>
-            <IoHome className="mb-1" /> Home /
-          </Link>
-        </small>
-        <small className="m-1">Daftar Penjualan</small>
-      </Col>
-      <ListPenjualan />
+      <MainLayout>
+        <BreadcrumbHeader title="Penjualan" />
+        <ListPenjualan />
+      </MainLayout>
     </>
   );
 };
