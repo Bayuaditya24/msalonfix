55f3935449aab3de558fa9f80125998ae42de616
I Putu Bayu Aditya Putra <49913455+Bayuaditya24@users.noreply.github.com>
2025-06-21 12:38:18 +0800

    Update DaftarBooking.js

--- frontend/src/pages/DaftarBooking.js
+++ frontend/src/pages/DaftarBooking.js
@@ -1,25 +1,14 @@
-import Navigation from "../components/Navigation";
-import { Link } from "react-router-dom";
-import { IoHome } from "react-icons/io5";
-import { Col } from "react-bootstrap";
 import ListBooking from "../components/booking/ListBooking";
+import BreadcrumbHeader from "../components/BreadcrumbHeader";
+import MainLayout from "../components/MainLayout";
 
 const DaftarBooking = () => {
   return (
     <>
-      <Navigation />
-      <Col
-        className="mb-3 p-2 card d-block"
-        style={{ background: "white", color: "grey" }}
-      >
-        <small className="m-1">
-          <Link style={{ textDecoration: "none", color: "grey" }} to={"/"}>
-            <IoHome className="mb-1" /> Home /
-          </Link>
-        </small>
-        <small className="m-1">Daftar Booking</small>
-      </Col>
-      <ListBooking />
+      <MainLayout>
+        <BreadcrumbHeader title="Daftar Booking" />
+        <ListBooking />
+      </MainLayout>
     </>
   );
 };
