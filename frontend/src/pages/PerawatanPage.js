7ad85cb5e6be39e3334a1386915328ac179731d8
I Putu Bayu Aditya Putra <49913455+Bayuaditya24@users.noreply.github.com>
2025-06-21 12:40:00 +0800

    Update PerawatanPage.js

--- frontend/src/pages/PerawatanPage.js
+++ frontend/src/pages/PerawatanPage.js
@@ -1,29 +1,19 @@
-import { Container, Col, Row, Card } from "react-bootstrap";
-import Navigation from "../components/Navigation";
-import { Link } from "react-router-dom";
-import { IoHome } from "react-icons/io5";
+import { Container } from "react-bootstrap";
+
 import ListCategory from "../components/perawatan/ListCategory";
+import BreadcrumbHeader from "../components/BreadcrumbHeader";
+import MainLayout from "../components/MainLayout";
 
 const PerawatanPage = () => {
   return (
     <>
-      <Navigation />
+      <MainLayout>
+        <BreadcrumbHeader title="Perawatan" />
 
-      <Col
-        className="mb-3 p-2 card d-block"
-        style={{ background: "white", color: "grey" }}
-      >
-        <small className="m-1">
-          <Link style={{ textDecoration: "none", color: "grey" }} to={"/"}>
-            <IoHome className="mb-1" /> Home /
-          </Link>
-        </small>
-        <small className="m-1">Perawatan</small>
-      </Col>
-
-      <Container fluid>
-        <ListCategory />
-      </Container>
+        <Container fluid>
+          <ListCategory />
+        </Container>
+      </MainLayout>
     </>
   );
 };
