31893745a760abe2ee0a10b32295ebd42eb7fa69
I Putu Bayu Aditya Putra <49913455+Bayuaditya24@users.noreply.github.com>
2025-06-21 12:38:35 +0800

    Update DaftarPelanggan.js

--- frontend/src/pages/DaftarPelanggan.js
+++ frontend/src/pages/DaftarPelanggan.js
@@ -1,25 +1,14 @@
-import Navigation from "../components/Navigation";
 import ListPelanggan from "../components/customer/ListPelanggan";
-import { Link } from "react-router-dom";
-import { IoHome } from "react-icons/io5";
-import { Col } from "react-bootstrap";
+import BreadcrumbHeader from "../components/BreadcrumbHeader";
+import MainLayout from "../components/MainLayout";
 
 const DaftarPelanggan = () => {
   return (
     <>
-      <Navigation />
-      <Col
-        className="mb-3 p-2 card d-block"
-        style={{ background: "white", color: "grey" }}
-      >
-        <small className="m-1">
-          <Link style={{ textDecoration: "none", color: "grey" }} to={"/"}>
-            <IoHome className="mb-1" /> Home /
-          </Link>
-        </small>
-        <small className="m-1">Daftar Pelanggan</small>
-      </Col>
-      <ListPelanggan />
+      <MainLayout>
+        <BreadcrumbHeader title="Daftar Pelanggan" />
+        <ListPelanggan />
+      </MainLayout>
     </>
   );
 };
