8fd5324f82a4f35653713e7f66b981f3c8278f5e
I Putu Bayu Aditya Putra <49913455+Bayuaditya24@users.noreply.github.com>
2025-06-21 12:39:23 +0800

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,40 +3,42 @@ import Navigation from "../components/Navigation";
 import { Link } from "react-router-dom";
 import { IoHome } from "react-icons/io5";
 import logobeaty from "../image/logobeaty.png";
+import MainLayout from "../components/MainLayout";
 
 const Home = () => {
   return (
     <>
-      <Navigation />
-      <Col
-        className="mb-3 p-2 card d-block"
-        style={{ background: "white", color: "grey" }}
-      >
-        <small className="m-1">
-          <Link style={{ textDecoration: "none", color: "grey" }} to={"/"}>
-            <IoHome className="mb-1" /> Home
-          </Link>
-        </small>
-      </Col>
-      <Container style={{ marginTop: "60px" }}>
-        <Card className="p-4 table-responsive">
-          <Col>
-            <Card className="p-2">
-              <div className="text-center">
-                <img
-                  src={logobeaty}
-                  style={{ width: "600px", height: "150px" }}
-                ></img>
-              </div>
-            </Card>
-          </Col>
-        </Card>
-      </Container>
-      <Container className="mt-3 text-center">
-        <small style={{ color: "grey" }}>
-          Jalan Raya By Pass Dharma Giri. Gianyar, Bali
-        </small>
-      </Container>
+      <MainLayout>
+        <Col
+          className="mb-3 p-2 card d-block"
+          style={{ background: "white", color: "grey" }}
+        >
+          <small className="m-1">
+            <Link style={{ textDecoration: "none", color: "grey" }} to={"/"}>
+              <IoHome className="mb-1" /> Home
+            </Link>
+          </small>
+        </Col>
+        <Container style={{ marginTop: "60px" }}>
+          <Card className="p-4 table-responsive">
+            <Col>
+              <Card className="p-2">
+                <div className="text-center">
+                  <img
+                    src={logobeaty}
+                    style={{ width: "600px", height: "150px" }}
+                  ></img>
+                </div>
+              </Card>
+            </Col>
+          </Card>
+        </Container>
+        <Container className="mt-3 text-center">
+          <small style={{ color: "grey" }}>
+            Jalan Raya By Pass Dharma Giri. Gianyar, Bali
+          </small>
+        </Container>
+      </MainLayout>
     </>
   );
 };
